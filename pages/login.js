b7350e1ae26ff5b31932a44f62995c7bd66160d0
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2025-08-28 09:39:31 -0500

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -27,7 +27,7 @@ export default function Login() {
   return (
     <div className="login-wrap">
       <header className="login-hero">
-        <img className="login-logo" src="https://storage.googleapis.com/mixo-sites/images/file-3ee255ce-ebaa-41de-96f6-a1233499cf70.png" alt="Manifestation Genie" />
+        <img className="login-logo" src="https://storage.googleapis.com/mixo-sites/images/file-a7eebac5-6af9-4253-bc71-34c0d455a852.png" alt="Manifestation Genie" />
         <h1 className="login-title">Your Personal AI Genie for Manifesting Dreams into Reality ✨💫</h1>
       </header>
 
