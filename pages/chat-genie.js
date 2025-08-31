4063630bdf1488a4a4c608a2a1deb47c094a4ebd
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2025-08-31 13:33:53 -0500

    Update chat-genie.js

--- pages/chat-genie.js
+++ pages/chat-genie.js
@@ -50,7 +50,7 @@ export default function ChatGenie() {
 
   async function callApi(text) {
     const state = get()
-    const resp = await fetch('/api/chat-genie', {
+    const resp = await fetch('/api/chat', {
       method:'POST',
       headers:{ 'Content-Type':'application/json' },
       body: JSON.stringify({
