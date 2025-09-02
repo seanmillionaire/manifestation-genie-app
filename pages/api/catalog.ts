8a94dd57c4fe7321f032362d18db88e59de75e2a
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2025-09-02 17:25:09 -0500

    Update catalog.ts

--- pages/api/catalog.ts
+++ pages/api/catalog.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import products from "@/src/engine/hm_products.json";
+// fallback
+import products from "../../../src/engine/hm_products.json";
+
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(products);
