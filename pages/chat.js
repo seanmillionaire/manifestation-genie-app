a0fe092934c6548bf36479480488329b6086e9b7
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2025-09-03 12:14:44 -0500

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,3 @@
 // pages/chat.js
-import ChatGenieScreen from "@/components/ChatGenie/ChatGenieScreen";
-export default function ChatPage() {
-  return <ChatGenieScreen />;
-}
+import ChatGenieScreen from "../components/ChatGenie/ChatGenieScreen";
+export default ChatGenieScreen;
