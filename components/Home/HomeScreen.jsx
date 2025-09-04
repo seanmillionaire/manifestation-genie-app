1e6cbcf3dd6825d295d1f947c9c6399f74c3c5ac
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2025-09-04 12:28:44 -0500

    Update HomeScreen.jsx

--- components/Home/HomeScreen.jsx
+++ components/Home/HomeScreen.jsx
@@ -182,7 +182,7 @@ const startManifesting = () => {
   return (
     <main style={{ width: "min(900px, 94vw)", margin: "30px auto" }}>
       <h1 style={{ fontSize: 28, fontWeight: 900, margin: "0 0 12px" }}>
-        👋 Welcome home, {firstName}. Let's begin
+        👋 Welcome home, {firstName}.
       </h1>
       <p
         style={{
