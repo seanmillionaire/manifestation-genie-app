d1a834f2233f552b8e817c98bbb12f238d2a559c
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2025-09-05 03:50:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 export default function Index() {
   const router = useRouter()
   useEffect(() => {
-    router.replace('/home')
+    router.replace('/login')
   }, [router])
   return null
 }
